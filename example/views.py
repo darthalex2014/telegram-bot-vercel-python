2d2b6335aae9806dd2bc423b8506f14480440579
darthalex2014 <51307259+darthalex2014@users.noreply.github.com>
2024-09-14 05:10:30 +0500

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,14 +1,13 @@
-from .bot import bot
 import asyncio
 import json
 from django.http import HttpResponse
-from .bot import bot_tele
+from .bot import bot, bot_tele  # Импортируем bot из bot.py
 
 def index(request):
     if request.method == 'POST':
         data = request.body
         res = json.loads(data.decode('utf-8'))
-        print(res)
+        print(res)  # Выводим полученные данные в консоль для отладки
         asyncio.run(bot_tele(res))
         return HttpResponse("ok")
     else:
